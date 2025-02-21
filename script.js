2ff212bf77b35bfa78948a6402afee81a515593a
Emy-Liberato <emilly.lliberato@gmail.com>
2025-02-20 23:19:32 -0300

    segunda aula - criando validações

--- script.js
+++ script.js
@@ -1,10 +1,56 @@
 const app = document.getElementById("app");
+const users = [
+  {
+    email: "test@test.com",
+    phone: "99999999999",
+    ref: 100,
+    refBy: null,
+  },
+  {
+    email: "tust@tust.com",
+    phone: "11123456789",
+    ref: 200,
+    refBy: 100,
+  },
+];
+
+const getUser = (userData) => {
+  return users.find((user) => {
+    return user.email == userData.email;
+  });
+};
+
+const showInvite = (userData) => {
+  app.innerHTML = `
+  <input type="text" id="link" value="https://evento.com" disabled>
+
+  <div id="stats">      
+      <h4>
+          80
+      </h4>
+      <p>
+          Inscrições feitas
+      </p>
+  </div>
+  `;
+};
 
 const formAction = () => {
   const form = document.getElementById("form");
   form.onsubmit = (event) => {
     event.preventDefault();
-    
+    const formData = new FormData(form);
+    const userData = {
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+    };
+
+    const user = getUser(userData);
+    if (user) {
+      showInvite(user);
+    } else {
+      // não encontrou o usuario
+    }
   };
 };
 
@@ -17,7 +63,7 @@ const startApp = () => {
         Confirmar
     </button>
     </form>
-    `;
+  `;
 
   app.innerHTML = content;
 
