66fff1d2bab771c89f518eb3853a400419ec4c10
Emy-Liberato <emilly.lliberato@gmail.com>
2025-02-21 21:51:30 -0300

    segunda aula - finalizando aplicação e iniciando estilização

--- script.js
+++ script.js
@@ -8,25 +8,36 @@ const users = [
   },
   {
     email: "tust@tust.com",
-    phone: "11123456789",
+    phone: "99999999999",
     ref: 200,
     refBy: 100,
   },
+  {
+    email: "tost@tost.com",
+    phone: "99999999999",
+    ref: 300,
+    refBy: 200,
+  }
 ];
 
 const getUser = (userData) => {
-  return users.find((user) => {
-    return user.email == userData.email;
-  });
+  return users.find((user) => user.email === userData.email);
 };
 
+// Contar indicações
+const getTotalSubscriber = (userData) => {
+  const subs = users.filter((user) => user.refBy === userData.ref);
+  return subs.length;
+}
+
+// Mensagem ao se inscrever
 const showInvite = (userData) => {
   app.innerHTML = `
   <input type="text" id="link" value="https://evento.com" disabled>
 
   <div id="stats">      
       <h4>
-          80
+          ${getTotalSubscriber(userData)}
       </h4>
       <p>
           Inscrições feitas
@@ -35,6 +46,20 @@ const showInvite = (userData) => {
   `;
 };
 
+// gerar número de ref de incrição
+const saveUser = (userData) => {
+  const newUser = {
+    ...userData, // Os três pontinhos fazem a cópia dos dados de userData
+    ref: Math.round(Math.random() * 4000), // Gerando um ref aleatório
+    refBy: 100, 
+  };
+
+  users.push(newUser);
+  console.log(users);
+  return newUser;
+};
+
+// Direciona para se inscrever/login 
 const formAction = () => {
   const form = document.getElementById("form");
   form.onsubmit = (event) => {
@@ -46,19 +71,21 @@ const formAction = () => {
     };
 
     const user = getUser(userData);
-    if (user) {
+    if (user) { // encontrou usuario
       showInvite(user);
-    } else {
-      // não encontrou o usuario
+    } else { // não encontrou o usuario
+      const newUser = saveUser(userData);
+      showInvite(newUser);
     }
   };
 };
 
+// Formulário HTML 
 const startApp = () => {
   const content = `
     <form id="form">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="text" name="phone" placeholder="Telefone">
+    <input type="email" name="email" placeholder="E-mail" required>
+    <input type="text" name="phone" placeholder="Telefone" required>
     <button>
         Confirmar
     </button>
@@ -71,3 +98,5 @@ const startApp = () => {
 };
 
 startApp();
+
+document.getElementById("logo").onclick = () => startApp()
